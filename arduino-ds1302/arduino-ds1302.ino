ec6ee5e933dc1a5145e212e3a8030195c4356ef9
Miguel Angel Salinas Gancedo <miguel@thingtrack.com>
2018-03-06 13:03:52 +0100

    Update arduino-ds1302.ino

--- arduino-ds1302/arduino-ds1302.ino
+++ arduino-ds1302/arduino-ds1302.ino
@@ -8,50 +8,48 @@
 #include <DS1302.h>
 
 namespace {
+  // Set the appropriate digital I/O pin connections. These are the pin
+  // assignments for the Arduino as well for as the DS1302 chip. See the DS1302
+  // datasheet:
+  //
+  //   http://datasheets.maximintegrated.com/en/ds/DS1302.pdf
+  const int kCePin   = 5;  // Chip Enable
+  const int kIoPin   = 6;  // Input/Output
+  const int kSclkPin = 7;  // Serial Clock
 
-// Set the appropriate digital I/O pin connections. These are the pin
-// assignments for the Arduino as well for as the DS1302 chip. See the DS1302
-// datasheet:
-//
-//   http://datasheets.maximintegrated.com/en/ds/DS1302.pdf
-const int kCePin   = 5;  // Chip Enable
-const int kIoPin   = 6;  // Input/Output
-const int kSclkPin = 7;  // Serial Clock
+  // Create a DS1302 object.
+  DS1302 rtc(kCePin, kIoPin, kSclkPin);
 
-// Create a DS1302 object.
-DS1302 rtc(kCePin, kIoPin, kSclkPin);
-
-String dayAsString(const Time::Day day) {
-  switch (day) {
-    case Time::kSunday: return "Sunday";
-    case Time::kMonday: return "Monday";
-    case Time::kTuesday: return "Tuesday";
-    case Time::kWednesday: return "Wednesday";
-    case Time::kThursday: return "Thursday";
-    case Time::kFriday: return "Friday";
-    case Time::kSaturday: return "Saturday";
+  String dayAsString(const Time::Day day) {
+    switch (day) {
+      case Time::kSunday: return "Sunday";
+      case Time::kMonday: return "Monday";
+      case Time::kTuesday: return "Tuesday";
+      case Time::kWednesday: return "Wednesday";
+      case Time::kThursday: return "Thursday";
+      case Time::kFriday: return "Friday";
+      case Time::kSaturday: return "Saturday";
+    }
+    return "(unknown day)";
   }
-  return "(unknown day)";
-}
 
-void printTime() {
-  // Get the current time and date from the chip.
-  Time t = rtc.time();
+  void printTime() {
+    // Get the current time and date from the chip.
+    Time t = rtc.time();
 
-  // Name the day of the week.
-  const String day = dayAsString(t.day);
+    // Name the day of the week.
+    const String day = dayAsString(t.day);
 
-  // Format the time and date and insert into the temporary buffer.
-  char buf[50];
-  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
-           day.c_str(),
-           t.yr, t.mon, t.date,
-           t.hr, t.min, t.sec);
+    // Format the time and date and insert into the temporary buffer.
+    char buf[50];
+    snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+             day.c_str(),
+             t.yr, t.mon, t.date,
+             t.hr, t.min, t.sec);
 
-  // Print the formatted string to serial so we can see the time.
-  Serial.println(buf);
-}
-
+    // Print the formatted string to serial so we can see the time.
+    Serial.println(buf);
+  }
 }  // namespace
 
 void setup() {
